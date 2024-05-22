bb3cb3139a58304f4a046baf3aa35835cfe927bd
Ishinaru <dvplima.cic@uesc.br>
2024-05-22 12:30:44 -0300

    fixing index.js from bot

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const telegramConfig = require("./config/telegram");
 const Bot = require("./bot");
 const TelegramBot = require("node-telegram-bot-api");
 
-const telegramBot = new TelegramBot(telegramConfig.token, {polling: true})
-const bot = new Bot(TelegramBot)
+const bot = new Bot(new TelegramBot(telegramConfig.token, {polling: true}))
+bot.execute()
